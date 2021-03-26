3b4ee998050b8eba6ded02515517b9086d1676f0
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-03-26 15:40:16 -0600

    updated npm

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({extended: false}))
 
 app.use(express.json())
 
-//app.use(express.static('public'))
+app.use(express.static('public'))
 
 app.use(express.static(path.join(__dirname, '/client/build')))
 
